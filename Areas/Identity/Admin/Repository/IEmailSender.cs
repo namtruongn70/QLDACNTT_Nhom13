cf8292a94e4ed99bab4bf78fda05c678acd2a9d7
namtruongn70 <namtruongn70@gmail.com>
2025-11-27 14:28:00 +0700

    Add files via upload

---
+++ Areas/Identity/Admin/Repository/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace CinemaManagement.Areas.Identity.Admin.Repository
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message); //hàm gửi email
+    }
+}
