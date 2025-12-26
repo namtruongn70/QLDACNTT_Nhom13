878cf48951b027bbd27bc93ac2f7cd94b63c47aa
namtruongn70 <namtruongn70@gmail.com>
2025-12-27 01:44:13 +0700

    Add files via upload

---
+++ ViewComponents/GenreDropdownViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class GenreDropdownViewComponent : ViewComponent
+{
+    private readonly AppDbContext _context;
+
+    public GenreDropdownViewComponent(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var genres = _context.Genres
+            .ToList();
+
+        return View(genres);
+    }
+}
---
+++ ViewComponents/TheaterDropdownViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using CinemaManagement.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CinemaManagement.ViewComponents
+{
+    public class TheaterDropdownViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public TheaterDropdownViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var theaters = _context.Theaters.ToList();
+            return View(theaters);
+        }
+    }
+}
